2a1d76c6342f246c4a98b817121ef53790426091
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-08-26 03:01:14 +0300

    Adding Subtraction
    The program reads a string in the form of \\d[+|-]+\\d and deals with consecutive minus and plus symbols

--- src/smartcalculator/SmartCalculator.java
+++ src/smartcalculator/SmartCalculator.java
@@ -1,59 +1,50 @@
-
 package smartcalculator;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SmartCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input;
-        String[] arr;
-        int a,b;
-        do{
-            input = scanner.nextLine();
-            arr = input.split(" ");
-            
-            // checking for keywords or case of single input
-            if(arr.length == 1){
-                if(input.equals("/exit")){
-                    System.out.println("Bye!");
-                    break;
-                }else if(input.equals("/help")){
-                    System.out.println("The program calculates the sum of numbers");
-                }else{
-                    try{
-                    a = Integer.parseInt(arr[0]);
-                    System.out.println(a);
-                }catch(NumberFormatException nfe){
-                    //nothing happens for now if there are string inputs
-                }
-                }
-            }else{
-                int[] numbers = new int[arr.length];//create an array in which we're going to put the string parsed to integers
-                boolean allok = true;
-                for (int i = 0; i<arr.length; i++ ){
-                    try{
-                        numbers[i] = Integer.parseInt(arr[i]);
-                    }catch(NumberFormatException nfe){
-                        allok = false;
-                        //nothing happens for now if there are string inputs
-                    }
-                }
-                
-                if (allok==true){
-                    int sum=0;
-                    for(int number:numbers){
-                        sum+=number;
-                    }
-                    System.out.println(sum);
-                }
-            }
-        }while (!input.equals("/exit"));  
+        String input = scanner.nextLine();
         
-        scanner.close(); 
-    }
+        String input1 = trimAndCut(input);
+        String input2 = replaceConsecutive(input1);
+        System.out.println(input2);
+        
+        
+        
+        
+
     }
     
+    private static String trimAndCut(String input){
+        Pattern pattern = Pattern.compile("\\s+");
+        Matcher matcher = pattern.matcher(input.trim());
+        return matcher.replaceAll("");
+    }
+
+    private static String replaceConsecutive(String input) {
+        //Replace even comsecutive minuses with a +
+        Pattern pattern = Pattern.compile("(\\-\\-)+");
+        Matcher matcher = pattern.matcher(input);
+        
+        String s1 = matcher.replaceAll("+");
+        
+        // We end up with "+-" which need to be replaced by minuses
+        pattern = Pattern.compile("\\+\\-");
+        matcher = pattern.matcher(s1);
+        
+        String s2 = matcher.replaceAll("-");
+        
+        // Dealing with consecutive +
+        pattern = Pattern.compile("\\++");
+        matcher = pattern.matcher(s2);
+        
+        return matcher.replaceAll("+");
+    }
+}
 
